c03d985db078b0b2f6c6ef591508b65719ba48df
Brad Haas <brad.haas@nbcuni.com>
2014-08-08 13:23:59 -0700

    hello world

--- hello.js
+++ hello.js
@@ -2,3 +2,15 @@
  * Created by 206438247 on 8/8/2014.
  */
 
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('*', function(req, res){
+  res.write("Hello World!");
+  res.end();
+});
+
+app.listen(port, function(){
+  console.log("Server Started");
+});
