74e0df5307d8cb5c37dd1cfa1f4577ee2cfeb241
Brad Haas <brad.haas@nbcuni.com>
2014-08-08 16:25:48 -0700

    started routing

--- hello.js
+++ hello.js
@@ -6,8 +6,13 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('*', function(req, res){
+app.get('/hi/', function(req, res){
   res.write("Hello World!");
+  res.end();
+});
+
+app.get('/bye/', function(req, res){
+  res.write("Cya!");
   res.end();
 });
 
